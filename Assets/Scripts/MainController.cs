c75ce215b6d459ce2e86a5f2e2ee1d4cbdabffb2
kidapu <kidapu@gmail.com>
2020-02-19 20:27:27 +0900

    Initial Commit

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RenderHeads.Media.AVProMovieCapture;
+
+public class MainController : MonoBehaviour
+{
+	[SerializeField]
+	Button _startRecButton;
+
+	[SerializeField]
+	Text _statusText;
+
+	[SerializeField]
+	CaptureFromCamera _cap;
+
+	bool _isRecording = false;
+
+	void Start()
+	{
+		_startRecButton.onClick.AddListener(() =>
+		{
+			Debug.LogFormat("Click 録画ボタン => isRecording : {0}", _isRecording);
+
+			if (!_isRecording)
+			{
+				_cap.StartCapture();
+				_isRecording = true;
+				_statusText.text = "録画中";
+			}
+			else
+			{
+				_cap.StopCapture();
+				_isRecording = false;
+				_statusText.text = "-";
+			}
+		});
+
+	}
+
+	void Update()
+	{
+
+	}
+
+
+}
---
+++ Assets/Scripts/RotateCam.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCam : MonoBehaviour
+{
+
+	void Update()
+	{
+		var rot = Mathf.Sin(Time.time * 2.5f) * 15f;
+		var rot2 = Mathf.Sin(Time.time * 1.5f) * 10f;
+		gameObject.transform.localEulerAngles = new Vector3(rot2,rot,0);
+	}
+}
